eb731387d3d14e706504ff8821a69eb1380ccead
ADRICE\rl53 <ruiyang.li@rice.edu>
2017-03-27 15:51:26 -0500

    Force to quit the process when error occurs in data dictionary built

--- data_filter.py
+++ data_filter.py
@@ -64,59 +64,60 @@ def filter_with_dict (data, note_col, id_field, filt_dict, cnty_filter = True):
 	
 
 def build_neighborhood_dict (input_shp, GeoIDfield, selection_type = "First_Order"):
-	arcpy.AddMessage("Start to build neighborhood dictonary...")
-	input_fields = arcpy.ListFields(input_shp)
-	id_type=""
-	duplicated=False
-	arcpy.AddMessage("Checking file for potential errors...")
-	for field in input_fields:
-		if field.name == GeoIDfield:
-			id_type = field.type
-			break
-	if id_type =="":
-		arcpy.AddError("GEOID field does not exist!!!")
-	sc = arcpy.SearchCursor(input_shp)
-	IDlist = dict()
-	row = sc.next()
-	while row is not None:
-		tempid = row.getValue(GeoIDfield)
-		if if_key_exist(tempid, IDlist):
-			duplicated=True
-			arcpy.AddError("Duplicate GeoIDs detected in Shapefile... Please Clean data first...")
-		else:	
-			IDlist[row.getValue(GeoIDfield)] = 1
+	try:
+		arcpy.AddMessage("Start to build neighborhood dictonary...")
+		input_fields = arcpy.ListFields(input_shp)
+		id_type=""
+		arcpy.AddMessage("Checking file for potential errors...")
+		for field in input_fields:
+			if field.name == GeoIDfield:
+				id_type = field.type
+				break
+		if id_type =="":
+			arcpy.AddError("GEOID field does not exist!!!")
+			raise ValueError
+		sc = arcpy.SearchCursor(input_shp)
+		IDlist = dict()
 		row = sc.next()
-	table_view = arcpy.MakeFeatureLayer_management(input_shp, "temp_tv")
-	del sc
-	del row
-	
-	arcpy.AddMessage("Searching for neighborhoods...")
-	neighbor_dict = dict()
-	i = 0
-	for geoid in IDlist:
-		temp_dict = dict()
-		if not id_type == "String":
-			arcpy.SelectLayerByAttribute_management(table_view,"NEW_SELECTION", GeoIDfield + " = " + str(geoid))
-		else:
-			arcpy.SelectLayerByAttribute_management(table_view,"NEW_SELECTION", GeoIDfield + " = '" + str(geoid)+"'")
-		if selection_type == "First_Order":
-			arcpy.SelectLayerByLocation_management(table_view,"INTERSECT", table_view, selection_type="ADD_TO_SELECTION")
-			sc = arcpy.SearchCursor(table_view)
+		while row is not None:
+			tempid = row.getValue(GeoIDfield)
+			if if_key_exist(tempid, IDlist):
+				arcpy.AddError("Duplicate GeoIDs detected in Shapefile... Please Clean data first...")
+				raise ValueError
+			else:	
+				IDlist[row.getValue(GeoIDfield)] = 1
 			row = sc.next()
-			while row is not None:
-				temp_dict[row.getValue(GeoIDfield)] = 1
+		table_view = arcpy.MakeFeatureLayer_management(input_shp, "temp_tv")
+		del sc
+		del row
+		
+		arcpy.AddMessage("Searching for neighborhoods...")
+		neighbor_dict = dict()
+		i = 0
+		for geoid in IDlist:
+			temp_dict = dict()
+			if not id_type == "String":
+				arcpy.SelectLayerByAttribute_management(table_view,"NEW_SELECTION", GeoIDfield + " = " + str(geoid))
+			else:
+				arcpy.SelectLayerByAttribute_management(table_view,"NEW_SELECTION", GeoIDfield + " = '" + str(geoid)+"'")
+			if selection_type == "First_Order":
+				arcpy.SelectLayerByLocation_management(table_view,"INTERSECT", table_view, selection_type="ADD_TO_SELECTION")
+				sc = arcpy.SearchCursor(table_view)
 				row = sc.next()
-		neighbor_dict[geoid] = temp_dict
-		if i % 100 == 0:
-			arcpy.AddMessage("%.2f" % (float(i)/len(IDlist)*100) + "% Done..." )
-		i += 1
-	
-	arcpy.Delete_management(table_view)
-	if duplicated:
-		arcpy.AddError("Please make sure your unique ID is unique!!!")
-	else:
+				while row is not None:
+					temp_dict[row.getValue(GeoIDfield)] = 1
+					row = sc.next()
+			neighbor_dict[geoid] = temp_dict
+			if i % 100 == 0:
+				arcpy.AddMessage("%.2f" % (float(i)/len(IDlist)*100) + "% Done..." )
+			i += 1
+		
+		arcpy.Delete_management(table_view)
 		arcpy.AddMessage("Neighborhood dictionary built successfully!!")
-	return neighbor_dict
+		return neighbor_dict
+	except:
+		arcpy.AddError("Please make sure your unique ID is unique!!!")	
+
 
 
 
