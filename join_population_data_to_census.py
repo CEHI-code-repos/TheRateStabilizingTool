c633261f930d76baa637bc8c0dd6b686a111a405
ADRICE\rl53 <ruiyang.li@rice.edu>
2017-12-15 17:12:21 -0600

    Add New function to use downloaded population data to create a population data enriched shapefile

---
+++ join_population_data_to_census.py
@@ -0,0 +1,103 @@
+import arcpy, os, json, sys
+import datetime as dt
+
+#raw_data = r'C:\\Users\rl53\Desktop\test\RST_Random_Seed_Test\RawData_state37_tract.data'
+#shp_container = r'C:\Users\rl53\Desktop\test\rst_test_join\tl_2010_37_tract10.shp'
+#shp_Geoid = 'GEOID10'
+#outputfolder = r'C:\Users\rl53\Desktop\test\rst_test_join'
+#raw_age = '0;20;40;60;-80'
+raw_data = arcpy.GetParameterAsText(0)
+shp_container = arcpy.GetParameterAsText(1)
+shp_Geoid = arcpy.GetParameterAsText(2)
+outputfolder = arcpy.GetParameterAsText(3)
+raw_age = str(arcpy.GetParameterAsText(4)) # Get age category, it's the lower bound of age group
+	
+sys.path.append(os.path.split(os.path.realpath(__file__))[0])	
+import fetch_data as fd # This module fetching data from Census Bureau
+fd = reload(fd) # Make sure newest module is loaded
+import construct_deathdata as cd # This module calculate rates from input data and fetched population data
+cd = reload(cd) # Make sure newest module is loaded
+
+#Most common fine age_structure = [0,1,2,5,6,9,10,12,15,18,20,25,30,35,40,45,50,55,60,65,70,75,80,85]
+# Format age category to construct an age category array for calculation
+split_age = raw_age.split(";")
+num_age = []
+for str_a in split_age:
+	num_age.append(int(str_a))
+age_structure = sorted(num_age)
+# This part of code enable a new feature in v0.9 - set an maximum age for oldest age category 
+if(age_structure[0] < 0):
+	cap = age_structure[0]
+	age_structure = age_structure[1:]
+	age_structure.append(cap)
+if(age_structure[0] < 0): # Maximum of one cap per request
+	raise ValueError("Please input one cap age only!!!")
+	
+# Read local data fetched from 1st step 
+part1_input=open(raw_data, 'r')
+#exec(part1_input.read())
+r_crit_level = part1_input.readline().replace("\n","")
+r_crit = part1_input.readline().replace("\n","")
+age_vector = json.loads(part1_input.readline().replace("\n","")) 
+age_exp = json.loads(part1_input.readline().replace("\n","")) 
+r_num_table = json.loads(part1_input.readline().replace("\n","").replace("\'", "\"")) 
+r_note_col = json.loads(part1_input.readline().replace("\n","").replace("\'", "\""))
+
+[result, percent] = fd.summarize_to_age_structure (age_vector, age_exp, r_num_table, r_note_col, age_structure)
+
+# Write population matrix, and standard population structure into files
+outfilename = "PopAge_structure_" + os.path.splitext(os.path.split(raw_data)[1])[0].split('_')[1] + '_'  + os.path.splitext(os.path.split(raw_data)[1])[0].split('_')[2] + ".csv"
+f = open(outputfolder + "\\" + outfilename, "w")
+head = True
+for row in result:
+	if head:
+		headerline = row
+		head = False
+	temp_text = cd.vect_to_str(row)
+	f.writelines(temp_text + "\n")
+f.close()
+
+# Update Schema.ini file
+f = open(outputfolder + "\\" + "schema.ini", "a")
+f.writelines("["+outfilename+"]\n")
+f.writelines("Format=CSVDelimited\n")
+f.writelines("ColNameHeader=True\n")
+
+i = 1
+for col in headerline:
+	#arcpy.AddMessage(col)
+	if col in ["NAME", "state", "county", "tract", "GEOID"]:
+		f.writelines("Col" + str(i) + "=" + str(col) + " Text Width 30\n")
+	elif col == "Alert":
+		f.writelines("Col" + str(i) + "=" + str(col) + " Text Width 100\n")
+	elif col == "Population":
+		f.writelines("Col" + str(i) + "=" + str(col) + " Long\n")
+	else:
+		f.writelines("Col" + str(i) + "=" + str(col) + " Double\n")
+	i += 1
+
+f.writelines("\n")
+f.close()
+
+temp_shp = arcpy.MakeFeatureLayer_management(shp_container, 'temp_shp')
+max_left = len(arcpy.Describe(temp_shp).fields)
+arcpy.AddJoin_management(temp_shp, shp_Geoid, outputfolder+'\\'+outfilename, 'GEOID')
+
+fms = arcpy.FieldMappings()
+
+fms_left = arcpy.FieldMappings()
+fms_left.addTable(temp_shp)
+i = 0
+for each_fm in fms_left:
+	if i < 	max(max_left-2,0):
+		fms.addFieldMap(each_fm)
+	elif each_fm.getInputFieldName(0).split('.')[-1] in age_structure:
+		temp = each_fm.outputField
+		temp.name = each_fm.getInputFieldName(0).split('.')[-1]
+		each_fm.outputField = temp
+		fms.addFieldMap(each_fm)
+		
+	i += 1
+	
+outshp = os.path.splitext(os.path.split(shp_container)[1])[0]+'_pop'
+arcpy.FeatureClassToFeatureClass_conversion(temp_shp, outputfolder, outshp, field_mapping = fms)
